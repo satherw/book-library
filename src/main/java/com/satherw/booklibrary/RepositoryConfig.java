c0d3ef4737f644d4be8ac2089373fcdd9b9a3ae8
Will Sather <satherw@vmware.com>
2022-07-28 22:35:38 -0400

    Restructure repo

--- src/main/java/com/satherw/booklibrary/RepositoryConfig.java
+++
@@ -1,8 +0,0 @@
-package com.satherw.booklibrary;
-
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@EnableJpaRepositories(basePackages = "com.satherw.booklibrary")
-public class RepositoryConfig {
-
-}
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO book (name, author) VALUES ('Refactoring (Second Edition)', 'Martin Fowler');
-INSERT INTO book (name, author) VALUES ('Extreme Programming Explained', 'Kent Beck');
